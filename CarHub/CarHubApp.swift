59f1d05bad393af4b89a52b0196d771c3e02e5d7
Yaroslav <Jarik239980@gmail.com>
2024-07-16 21:30:33 +0300

    Initial Commit

---
+++ CarHub/CarHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarHubApp.swift
+//  CarHub
+//
+//  Created by Yaroslav on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CarHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
